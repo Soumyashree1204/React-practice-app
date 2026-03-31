6e9d40e2d810c47bc1fa103a680e28b285d40381
Soumyashree Dash <soumyashreedash03@gmail.com>
2026-03-31 11:13:32 +0530

    updated code

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,23 +1,20 @@
 import './App.css'
-import Header from './Header'
-// import Welcome from './Welcome'
+import Welcome from './Welcome'
 
-// function hello()
-// {
-//   alert("Hello,I am Soumyashree")
-// }
+function hello()
+{
+  alert("Hello,I am Soumyashree")
+}
 
 function App() {
  
   return (
     <>
       <h1>Hello</h1>
-      {/* <Welcome /> */}
+      <Welcome />
       <h3>My Name is Soumyashree</h3>
-      <Header/>
-      
 
-      {/* <button onClick={hello} >Click here!</button> */}
+      <button onClick={hello} >Click here!</button>
     </>
   )
 }
